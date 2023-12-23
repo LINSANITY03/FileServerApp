1b48f0c573ce2f278144b2e42fad3b61cb707838
pujan tamang <pujantamang92@gmail.com>
2023-12-23 15:52:18 +0000

    remove the flag help as usedefault -h instead

--- helper.go
+++ helper.go
@@ -2,19 +2,17 @@ package main
 
 import (
 	"file_transfer_protocol/file_components"
-	"fmt"
 	"os"
 )
 
 // This functions handles logic behind argu or flag of cli commands.
-func argHandler(filepath *string, useHelp *bool, useQuit *bool) {
+func argHandler(filepath *string, useQuit *bool) {
 
 	// if flag.NArg() == 0 || flag.Arg(0) != "fsa" {
 	// 	fmt.Println("correct command required")
 	// 	os.Exit(1)
 	// }
 
-	helpFlag(useHelp)
 	quitFlag(useQuit)
 	file_components.ReadFileFromPath(filepath)
 	// if *filename == " " {
@@ -23,22 +21,6 @@ func argHandler(filepath *string, useHelp *bool, useQuit *bool) {
 	// } else {
 	// 	fmt.Println("This is the file path:", *filename)
 	// }
-}
-
-// logic when user enter -help or --help
-func helpFlag(useHelp *bool) {
-	if *useHelp {
-		// logic if user enter "-help or --help arguments"
-		fmt.Println(
-			`
-			This is the default help message.
-			Syntax:
-			-help:
-				Shows the available syntax for this cli.
-			-q:
-				Quits the cli.
-			`)
-	}
 }
 
 // this function when called exits the cli
--- main.go
+++ main.go
@@ -6,22 +6,21 @@ import (
 
 // Main entrypoint functions of the file.
 func main() {
-	filepath, useHelp, useQuit := inputParams()
+	filepath, useQuit := inputParams()
 
 	flag.Parse()
 
-	argHandler(filepath, useHelp, useQuit)
+	argHandler(filepath, useQuit)
 
 }
 
 // responsible for setting variable for flag
-func inputParams() (*string, *bool, *bool) {
+func inputParams() (*string, *bool) {
 
 	filepath := flag.String("file", " ", "Path of a file")
-	useHelp := flag.Bool("help", false, "display available syntax")
 	useQuit := flag.Bool("q", false, "Exit the program")
 	// whenever we use flag it is always a pointer ("*")
 	// *useHelp returns false
 	// useHelp returns address 0xc00000a0c6
-	return filepath, useHelp, useQuit
+	return filepath, useQuit
 }
